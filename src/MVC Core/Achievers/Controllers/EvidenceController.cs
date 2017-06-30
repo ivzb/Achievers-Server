949a8a141f8e72666246ad29a8cfa064ce03f549
ivzb <izahariev96@gmail.com>
2017-06-30 14:06:22 +0300

    :construction: added Evidence controller

---
+++ src/MVC Core/Achievers/Controllers/EvidenceController.cs
@@ -0,0 +1,36 @@
+﻿using Achievers.Infrastructure.Extensions;
+using Achievers.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Achievers.Controllers
+{
+    [Authorize]
+    public class EvidenceController : Controller
+    {
+        private readonly IEvidenceService evidence;
+        private readonly IAchievementsService achievements;
+
+        public EvidenceController(IEvidenceService evidence, IAchievementsService achievements)
+        {
+            this.evidence = evidence;
+            this.achievements = achievements;
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return await this.JsonOrNotFound(async () => await this.evidence.FindAsync(id));
+        }
+
+        public async Task<IActionResult> LoadByAchievement(int achievementId)
+        {
+            if (!(await this.achievements.ExistAsync(achievementId)))
+            {
+                return this.NotFound(achievementId);
+            }
+
+            return await this.JsonOrNotFound(async () => await this.evidence.LoadByAchievementAsync(achievementId));
+        }
+    }
+}
